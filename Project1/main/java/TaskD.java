dee45d4d64a152cf9a2756927bf96f0fa6a10364
Sarayu Vijayanagaram <147265985+sarayuv@users.noreply.github.com>
2025-02-05 17:43:52 -0500

    Update and rename ConnectednessFactor.java to TaskD.java

--- Project1/main/java/TaskD.java
+++ Project1/main/java/TaskD.java
@@ -1,18 +1,11 @@
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import java.io.IOException;
 
-// TASK D
-
-public class ConnectednessFactor {
-    public static class ConnectednessFactorMapper extends Mapper<Object, Text, Text, IntWritable> {
+public class TaskD {
+    public static class TaskDMapper extends Mapper<Object, Text, Text, IntWritable> {
         private final IntWritable one = new IntWritable(1);
 
         @Override
@@ -25,7 +18,8 @@ public class ConnectednessFactor {
             }
         }
     }
-    public static class ConnectednessFactorReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    public static class TaskDReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
         @Override
         public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             int sum = 0;
