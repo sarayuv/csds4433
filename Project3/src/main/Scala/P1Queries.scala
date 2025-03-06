71e1e72ebe0f53558c6b6e1e8734c847bef22f71
Sarayu Vijayanagaram <147265985+sarayuv@users.noreply.github.com>
2025-03-06 18:20:30 -0500

    Update P1Queries.scala

--- Project3/src/main/Scala/P1Queries.scala
+++ Project3/src/main/Scala/P1Queries.scala
@@ -1,9 +1,9 @@
 import org.apache.spark.{SparkConf, SparkContext}
 
-object Queries {
+object P1Queries {
   def main(args: Array[String]): Unit = {
 
-    val conf = new SparkConf().setAppName("Query1").setMaster("local[*]")
+    val conf = new SparkConf().setAppName("Query").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
     // load the Meta-Event file into an RDD
@@ -36,7 +36,7 @@ object Queries {
     }.filter { case (_, _, _, test) => test == "sick" }
 
     // save results
-    val outputDir1 = "output/sick_people"
+    val outputDir1 = "output/Problem1/query1"
     sickPeopleRDD.map { case (id, name, table, test) =>
       s"$id,$name,$table,$test" // convert to CSV format
     }.saveAsTextFile(outputDir1)
@@ -45,54 +45,51 @@ object Queries {
     // Query 2
     val idRDD = mndDataRDD.map { line =>
       val col = line.split(",")
-      (col(0), col(1), col(2)) // (id, name, table)
+      (col(0), (col(1), col(2))) // (id, (name, table))
     }
 
-    val illRDD = reportedIllnessesRDD.map { line =>
+    val illRDD = riDataRDD.map { line =>
       val col = line.split(",")
       (col(0), col(1)) // (id, test)
     }
 
     val join2RDD = idRDD.join(illRDD).map {
-      case (id, name, table) => (id, name, table)
+      case (id, ((name, table), test)) => (id, name, table, test)
     }
 
     // save results
-    val outputDir2 = "output/query2"
-    join2RDD.map { case (id, name, table) =>
-      s"$id,$name,$table" // convert to CSV format
+    val outputDir2 = "output/Problem1/query2"
+    join2RDD.map { case (id, name, table, test) =>
+      s"$id,$name,$table,$test" // convert to CSV format
     }.saveAsTextFile(outputDir2)
 
 
     // Query 3
     val tableRDD = mDataRDD.map { line =>
       val columns = line.split(",")
-      (columns(0), columns(1), columns(2), columns(3)) // (id, name, table, test)
-    }.filter { case (_, _, _, test) => test == "sick"
-    }.map { case (_, _, table, _) => (table, 1)}
+      (columns(2), 1) // (table, 1)
+    }.filter { case (_, test) => test == "sick" }
 
     val notSickRDD = mDataRDD.map { line =>
       val columns = line.split(",")
-      (columns(0), columns(1), columns(2), columns(3)) // (id, name, table, test)
-    }.filter { case (_, _, _, test) => test == "not-sick"
-    }.map { case (id, name, table, _) => (table, (id, name))}
+      (columns(2), (columns(0), columns(1))) // (table, (id, name))
+    }.filter { case (_, test) => test == "not-sick" }
 
     val sickTablejoin = tableRDD.join(notSickRDD).map {
-      case (_, id, name, _) => (id, name)
+      case (table, (_, (id, name))) => (id, name)
     }.distinct()
 
-    // save results
-    val outputDir3 = "output/query3"
+    // Save results
+    val outputDir3 = "output/Problem1/query3"
     sickTablejoin.map { case (id, name) =>
-      s"$id,$name" // convert to CSV format
+      s"$id,$name" // Convert to CSV format
     }.saveAsTextFile(outputDir3)
 
 
     // Query 4
     val healthyTableRDD = mDataRDD.map { line =>
       val columns = line.split(",")
-      (columns(0), columns(1), columns(2), columns(3)) // (id, name, table, test)
-    }.map { case (_, _, table, test) => test => (table, test)
+      (columns(2), columns(3)) // (table, test)
     }.groupByKey().map { case (table, tests) =>
       val sick = tests.exists(_ == "sick")
       val num = tests.size
@@ -101,12 +98,13 @@ object Queries {
     }
 
     // save results
-    val outputDir4 = "output/query4"
-    Tablejoin.map { case (table, num, flag) =>
+    val outputDir4 = "output/Problem1/query4"
+    healthyTableRDD.map { case (table, num, flag) =>
       s"$table,$num,$flag" // convert to CSV format
     }.saveAsTextFile(outputDir4)
 
-// Query 5
+
+    // Query 5
 
 
     sc.stop()
